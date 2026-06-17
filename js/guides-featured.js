7da9b44137c80fe96f6c7627a0d6d99ed477cfd0
sglopez327-cmyk <sglopez327@gmail.com>
2026-06-18 00:25:46 +0200

    Mejorar visibilidad de botones CTA

--- js/guides-featured.js
+++ js/guides-featured.js
@@ -106,7 +106,7 @@
       '<p class="guide-featured-card__desc">' +
       descripcion +
       '</p>' +
-      '<span class="guide-featured-card__cta">' +
+      '<span class="guide-featured-card__cta cta-button-highlight">' +
       'Leer guía' +
       '<svg width="18" height="18" viewBox="0 0 24 24" fill="none" aria-hidden="true">' +
       '<path d="M5 12h14M13 6l6 6-6 6" stroke="currentColor" stroke-width="1.8" stroke-linecap="round" stroke-linejoin="round"></path>' +
